e4da0181d2bfde388894e35cec927a46909b3b7e
kobbi7 <charls089@gmail.com>
2019-10-23 14:16:29 +0900

    MapView changed (activity → dialog)

--- app/src/main/java/com/kobbi/weather/info/ui/view/activity/AddPlaceActivity.kt
+++ app/src/main/java/com/kobbi/weather/info/ui/view/activity/AddPlaceActivity.kt
@@ -1,72 +1,52 @@
 package com.kobbi.weather.info.ui.view.activity
 
 import android.content.DialogInterface
-import android.location.Location
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.AlertDialog
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.kobbi.weather.info.R
 import com.kobbi.weather.info.databinding.ActivityAddPlaceBinding
-import com.kobbi.weather.info.presenter.listener.LocationListener
-import com.kobbi.weather.info.presenter.location.LocationManager
 import com.kobbi.weather.info.presenter.viewmodel.PlaceViewModel
+import com.kobbi.weather.info.ui.view.dialog.MapViewDialog
 import com.kobbi.weather.info.ui.view.dialog.SelectPlaceDialog
+import com.kobbi.weather.info.util.DLog
 import com.kobbi.weather.info.util.LocationUtils
 import com.kobbi.weather.info.util.Utils
 
-class AddPlaceActivity : AppCompatActivity(), OnMapReadyCallback {
-    private var mGoogleMap: GoogleMap? = null
+class AddPlaceActivity : AppCompatActivity() {
     private var mPlaceVm: PlaceViewModel? = null
     private var mIsMultiChoice: Boolean = false
     private var mIsLimit: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityAddPlaceBinding>(
+        DataBindingUtil.setContentView<ActivityAddPlaceBinding>(
             this, R.layout.activity_add_place
-        )
-        mPlaceVm = ViewModelProviders.of(this)[PlaceViewModel::class.java].apply {
-            isMultiCheck.observe(this@AddPlaceActivity,
-                Observer { isMulti ->
-                    mIsMultiChoice = isMulti
-                    invalidateOptionsMenu()
+        ).run {
+            mPlaceVm = ViewModelProviders.of(this@AddPlaceActivity)[PlaceViewModel::class.java].apply {
+                isMultiCheck.observe(this@AddPlaceActivity,
+                    Observer { isMulti ->
+                        mIsMultiChoice = isMulti
+                        invalidateOptionsMenu()
+                    })
+                clickPosition.observe(this@AddPlaceActivity, Observer { address ->
+                    val latLng = LocationUtils.convertAddress(applicationContext, address)
+                    MapViewDialog(latLng).show(supportFragmentManager, SelectPlaceDialog.TAG)
+                    DLog.d(javaClass, "clickPosition.observe() --> latLng : $latLng")
                 })
-            clickPosition.observe(this@AddPlaceActivity, Observer { address ->
-                val latLng = LocationUtils.convertAddress(applicationContext, address)
-                setMapView(latLng)
-            })
-            place.observe(this@AddPlaceActivity, Observer {
-                mIsLimit = it.size >= 5
-            })
-        }
-        binding.placeVm = mPlaceVm
-        binding.lifecycleOwner = this
-        val mapView = supportFragmentManager.findFragmentById(R.id.fragment_map_view)
-        mapView?.let { mapFragment ->
-            if (mapFragment is SupportMapFragment) mapFragment.getMapAsync(this)
-        }
-    }
-
-    override fun onMapReady(googleMap: GoogleMap?) {
-        mGoogleMap = googleMap
-        LocationManager.getLocation(applicationContext, object : LocationListener {
-            override fun onComplete(responseCode: Int, location: Location?) {
-                location?.let {
-                    setMapView(LatLng(it.latitude, it.longitude))
-                } ?: setMapView(LatLng(37.5642135, 127.0016985))
+                place.observe(this@AddPlaceActivity, Observer {
+                    mIsLimit = it.size >= 5
+                })
             }
-        })
+            placeVm = mPlaceVm
+            lifecycleOwner = this@AddPlaceActivity
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -92,10 +72,7 @@ class AddPlaceActivity : AppCompatActivity(), OnMapReadyCallback {
                         })
                     return false
                 }
-                SelectPlaceDialog().show(
-                    supportFragmentManager,
-                    SelectPlaceDialog.TAG
-                )
+                SelectPlaceDialog().show(supportFragmentManager, SelectPlaceDialog.TAG)
             }
             R.id.action_remove_place -> {
                 Utils.showAlertDialog(
@@ -113,18 +90,5 @@ class AddPlaceActivity : AppCompatActivity(), OnMapReadyCallback {
             }
         }
         return super.onOptionsItemSelected(item)
-    }
-
-    private fun setMapView(latLng: LatLng?) {
-        mGoogleMap?.let { googleMap ->
-            latLng?.let {
-                val markerOptions = MarkerOptions().apply {
-                    position(latLng)
-                }
-                googleMap.clear()
-                googleMap.addMarker(markerOptions)
-                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
-            }
-        }
     }
 }
---
+++ app/src/main/java/com/kobbi/weather/info/ui/view/dialog/MapViewDialog.kt
@@ -0,0 +1,69 @@
+package com.kobbi.weather.info.ui.view.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.FrameLayout
+import androidx.fragment.app.DialogFragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.kobbi.weather.info.R
+
+class MapViewDialog(private val latLng: LatLng?) : DialogFragment(), OnMapReadyCallback {
+    companion object {
+        const val TAG = "MapViewDialog"
+    }
+
+    private var mGoogleMap: GoogleMap? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.dialog_map_vew, null, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.startAnimation(AnimationUtils.loadAnimation(context, R.anim.fade_in))
+        fragmentManager?.findFragmentById(R.id.fragment_map_view)?.let { mapFragment ->
+            if (mapFragment is SupportMapFragment)
+                mapFragment.getMapAsync(this)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val params = dialog?.window?.attributes?.apply {
+            width = FrameLayout.LayoutParams.MATCH_PARENT
+            height = 1000
+        }
+        dialog?.window?.attributes = params as android.view.WindowManager.LayoutParams
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        fragmentManager?.run {
+            findFragmentById(R.id.fragment_map_view)?.let { mapFragment ->
+                if (mapFragment is SupportMapFragment)
+                    beginTransaction().remove(mapFragment).commit()
+            }
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        mGoogleMap = googleMap
+        setMapView(latLng)
+    }
+
+    private fun setMapView(latLng: LatLng?) {
+        mGoogleMap?.let { googleMap ->
+            latLng?.let {
+                googleMap.clear()
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+            }
+        }
+    }
+}
