64f601bf878a6201c01a3a60133de103f2096846
kobbi7 <charls089@gmail.com>
2019-11-14 22:20:53 +0900

    bug fix : Foreground

--- app/src/main/java/com/kobbi/weather/info/data/network/client/AirMeasureClient.kt
+++ app/src/main/java/com/kobbi/weather/info/data/network/client/AirMeasureClient.kt
@@ -2,10 +2,12 @@ package com.kobbi.weather.info.data.network.client
 
 import com.kobbi.weather.info.data.network.api.AirMeasureApi
 import com.kobbi.weather.info.util.ApiConstants
+import kotlinx.coroutines.withTimeout
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class AirMeasureClient private constructor() {
     companion object {
@@ -24,12 +26,17 @@ class AirMeasureClient private constructor() {
             val interceptor = HttpLoggingInterceptor().apply {
                 level = HttpLoggingInterceptor.Level.BODY
             }
-            val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+            val client = OkHttpClient.Builder().run {
+                connectTimeout(30, TimeUnit.SECONDS)
+                addInterceptor(interceptor)
+                build()
+            }
 
             return Retrofit.Builder().apply {
                 baseUrl(ApiConstants.API_AIRKOREA_BASE_URL)
                 addConverterFactory(GsonConverterFactory.create())
                 client(client)
+
             }.build().create(AirMeasureApi::class.java)
         }
     }
