51e1a83fa8c88c2efd2a2e5213858e8c7181717c
kobbi7 <charls089@gmail.com>
2019-12-13 08:07:00 +0900

    bug fix : run in thread

--- app/src/main/java/com/kobbi/weather/info/presenter/viewmodel/WidgetViewModel.kt
+++ app/src/main/java/com/kobbi/weather/info/presenter/viewmodel/WidgetViewModel.kt
@@ -1,6 +1,7 @@
 package com.kobbi.weather.info.presenter.viewmodel
 
 import android.content.Context
+import android.util.Log
 import com.kobbi.weather.info.data.database.entity.Area
 import com.kobbi.weather.info.presenter.listener.CompleteListener
 import com.kobbi.weather.info.presenter.model.data.WeatherInfo
@@ -8,6 +9,7 @@ import com.kobbi.weather.info.presenter.model.type.OfferType
 import com.kobbi.weather.info.presenter.model.type.ReturnCode
 import com.kobbi.weather.info.presenter.repository.ApiRequestRepository
 import com.kobbi.weather.info.presenter.repository.WeatherRepository
+import kotlin.concurrent.thread
 
 class WidgetViewModel(private val context: Context) {
 
@@ -17,31 +19,34 @@ class WidgetViewModel(private val context: Context) {
     private val weatherInfo: WeatherInfo? get() = weatherRepository.loadWeatherInfo(locatedArea)
 
     fun getWeatherInfo(listener: CompleteListener) {
-        locatedArea?.run {
-            weatherInfo?.run {
-                listener.onComplete(ReturnCode.NO_ERROR, this)
-            } ?: ApiRequestRepository.initBaseAreaData(context, this, object : CompleteListener {
-                override fun onComplete(code: ReturnCode, data: Any) {
-                    when(code) {
-                        ReturnCode.NO_ERROR -> {
-                            if (data is OfferType)
-                                when (data) {
-                                    OfferType.CURRENT, OfferType.MIN_MAX -> {
-                                        weatherInfo?.run {
-                                            listener.onComplete(ReturnCode.NO_ERROR, this)
+        thread {
+            locatedArea?.run {
+                weatherInfo?.run {
+                    listener.onComplete(ReturnCode.NO_ERROR, this)
+                } ?: ApiRequestRepository.initBaseAreaData(context, this, object : CompleteListener {
+                    override fun onComplete(code: ReturnCode, data: Any) {
+                        Log.e("####", "WidgetViewModel.onComplete() --> code : $code, data : $data")
+                        when (code) {
+                            ReturnCode.NO_ERROR -> {
+                                if (data is OfferType)
+                                    when (data) {
+                                        OfferType.CURRENT, OfferType.MIN_MAX -> {
+                                            weatherInfo?.run {
+                                                listener.onComplete(ReturnCode.NO_ERROR, this)
+                                            }
+                                        }
+                                        else -> {
+                                            //Nothing
                                         }
                                     }
-                                    else -> {
-                                        //Nothing
-                                    }
-                                }
-                        }
-                        else -> {
-                            listener.onComplete(code, data)
+                            }
+                            else -> {
+                                listener.onComplete(code, data)
+                            }
                         }
                     }
-                }
-            })
-        } ?: listener.onComplete(ReturnCode.DATA_IS_NULL, "We can't find your area right now.")
+                })
+            } ?: listener.onComplete(ReturnCode.DATA_IS_NULL, "We can't find your area right now.")
+        }
     }
 }
